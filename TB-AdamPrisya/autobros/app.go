1710cc9082658e3e33e1121727ef3732e0f0eac4
adam-ghafara <adam.ghafara.job@gmail.com>
2023-10-20 16:25:41 +0700

    Tambah Mongo

--- TB-AdamPrisya/autobros/app.go
+++ TB-AdamPrisya/autobros/app.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"autobros/controls/kendaraancontroller"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
-		router.HandleFunc("/", kendaraancontroller.Dashboard).Methods("GET")
-		router.HandleFunc("/autobros", kendaraancontroller.Dashboard).Methods("GET")
-		router.HandleFunc("/autobros/tables", kendaraancontroller.Tables).Methods("GET")
-		router.HandleFunc("/autobros/tambahservis", kendaraancontroller.Forms).Methods("GET")
-		router.HandleFunc("/autobros/tambahservis", kendaraancontroller.Forms).Methods("POST")
-		router.HandleFunc("/autobros/tables/updateservis", kendaraancontroller.FormAdmin).Methods("GET")
-		router.HandleFunc("/autobros/tables/updateservis", kendaraancontroller.FormAdmin).Methods("POST")
+	router.HandleFunc("/", kendaraancontroller.Dashboard).Methods("GET")
+	router.HandleFunc("/autobros", kendaraancontroller.Dashboard).Methods("GET")
+	router.HandleFunc("/autobros/tables", kendaraancontroller.Tables).Methods("GET")
+	router.HandleFunc("/autobros/tambahservis", kendaraancontroller.Forms).Methods("GET")
+	router.HandleFunc("/autobros/tambahservis", kendaraancontroller.Forms).Methods("POST")
+	router.HandleFunc("/autobros/tables/updateservis", kendaraancontroller.FormAdmin).Methods("GET")
+	router.HandleFunc("/autobros/tables/updateservis", kendaraancontroller.FormAdmin).Methods("POST")
 	// no method functional
-		router.HandleFunc("/autobros/tables/hapusservis", kendaraancontroller.Delete)
-		router.HandleFunc("/autobros/tables/detaildata/hapusservis", kendaraancontroller.Delete)
-		router.HandleFunc("/autobros/tables/detaildata", kendaraancontroller.Details)
+	router.HandleFunc("/autobros/tables/hapusservis", kendaraancontroller.Delete)
+	router.HandleFunc("/autobros/tables/detaildata/hapusservis", kendaraancontroller.Delete)
+	router.HandleFunc("/autobros/tables/detaildata", kendaraancontroller.Details)
 	http.ListenAndServe(":8000", router)
 }
